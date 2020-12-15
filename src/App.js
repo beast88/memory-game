9b20fafe37ea69e33566c27d25e389ab8ae3433d
Paul Rogers <progers999@hotmail.co.uk>
2020-12-15 18:46:53 +0000

    Shuffling function added to return 3 random characters

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import Header from './Components/Header'
 import Assets from './Assets/Assets'
 import About from './Components/About'
+import Character from './Components/Character'
 
 const App = () => {
   const [score, setScore] = useState(0)
@@ -15,6 +16,25 @@ const App = () => {
   const resetGame = () => {
     setBestScore(score)
     setScore(0)
+  }
+
+  const shuffle = (arr) => {
+    let currentIndex = arr.length, tempValue, randomIndex
+
+    while(0 !== currentIndex){
+      randomIndex = Math.floor(Math.random() * currentIndex--)
+
+      tempValue = arr[currentIndex]
+      arr[currentIndex] = arr[randomIndex]
+      arr[randomIndex] = tempValue
+    }
+
+    return arr
+  }
+
+  const characterGen = () => {
+    let random = shuffle(Assets)
+    console.log(random.slice(0, 3))
   }
 
   return(
---
+++ src/Components/Character.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Character = () => {
+	return(
+		<div className="character-card">
+			<h1>Hello World!!</h1>
+		</div>
+	)
+}
+
+export default Character
