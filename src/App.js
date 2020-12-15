41ee2ff2d714bf52b54004ecbc9abe7621ec7755
Paul Rogers <progers999@hotmail.co.uk>
2020-12-15 16:26:02 +0000

    Score card constructed

--- src/App.js
+++ src/App.js
@@ -4,11 +4,19 @@ import Assets from './Assets/Assets'
 import About from './Components/About'
 
 const App = () => {
+  const [score, setScore] = useState(0)
+  const [bestScore, setBestScore] = useState(0)
+  const [guess, setGuess] = useState([])
 
   return(
     <div className="container">
       <Header />
       <About />
+
+      <div className="score-card">
+        <h3>Current Score: {score}</h3>
+        <h3>Best Score: {bestScore}</h3>
+      </div>
     </div>
   )
 }
