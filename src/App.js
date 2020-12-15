f91653e8499b88aec63514c444fdaa7b54c3b16e
Paul Rogers <progers999@hotmail.co.uk>
2020-12-15 15:51:29 +0000

    About section constructed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, {useState} from 'react'
 import Header from './Components/Header'
 import Assets from './Assets/Assets'
+import About from './Components/About'
 
 const App = () => {
 
   return(
     <div className="container">
       <Header />
+      <About />
     </div>
   )
 }
---
+++ src/Components/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+	return (
+		<div className="about-container">
+			<p>The aim of the game is simple. You will be shown three characters at random. Click on a character to score a point. You cannot click the same character twice, otherwise you must start again. Aim for the highest score of 35!! Good Luck!</p>
+		</div>
+	)
+}
+
+export default About
