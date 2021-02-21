4099ab0a9eb0a34c331eafd79615a77f2868f74a
Paul Rogers <progers999@hotmail.co.uk>
2021-02-21 16:54:21 +0000

    Updates

---
+++ precache-manifest.1e2a93486a9ca02277e7f368ff80a7c8.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = [
+  {
+    "revision": "45803ae627dc889561f8",
+    "url": "/memory-game/static/js/main.8d92f8e3.chunk.js"
+  },
+  {
+    "revision": "799512f57ffe7b17c145",
+    "url": "/memory-game/static/js/runtime~main.4bfb86a8.js"
+  },
+  {
+    "revision": "6522df5ac5af29293029",
+    "url": "/memory-game/static/js/2.b8ce003f.chunk.js"
+  },
+  {
+    "revision": "050def35a9f8beb79a2e8f6e5a4510a5",
+    "url": "/memory-game/static/media/Aqua.050def35.png"
+  },
+  {
+    "revision": "04c30dd3a05791edaf168e800b9ab684",
+    "url": "/memory-game/static/media/Barrett.04c30dd3.jpg"
+  },
+  {
+    "revision": "88e8c05859492d08e9810d782144e8bc",
+    "url": "/memory-game/static/media/Batman.88e8c058.jpg"
+  },
+  {
+    "revision": "d5360d369106f73227860c2511b6ac93",
+    "url": "/memory-game/static/media/Bowser.d5360d36.png"
+  },
+  {
+    "revision": "cb381acac572b6eeb8f7ca9fc0935739",
+    "url": "/memory-game/static/media/Cassiopeia.cb381aca.jpg"
+  },
+  {
+    "revision": "cc7cd1ddbb4303557b0054fcceb58833",
+    "url": "/memory-game/static/media/Catwoman.cc7cd1dd.jpg"
+  },
+  {
+    "revision": "9a1452f18693f67e41400a0d17ddc46d",
+    "url": "/memory-game/static/media/Chase.9a1452f1.jpg"
+  },
+  {
+    "revision": "c54f18923a2fa6bf567249393b4bf8ad",
+    "url": "/memory-game/static/media/Cube.c54f1892.jpg"
+  },
+  {
+    "revision": "86e696aa90502426d684d13f2fe44244",
+    "url": "/memory-game/static/media/Dante.86e696aa.jpg"
+  },
+  {
+    "revision": "dfffc423dea90cc01d4a049d3056b74c",
+    "url": "/memory-game/static/media/Donald.dfffc423.jpg"
+  },
+  {
+    "revision": "521cb08a9d5155b40520cd716e94716c",
+    "url": "/memory-game/static/media/Drake.521cb08a.jpg"
+  },
+  {
+    "revision": "4dca531cdc4dae6048a6684af7abd11b",
+    "url": "/memory-game/static/media/Glados.4dca531c.jpg"
+  },
+  {
+    "revision": "6bb33165442606df0802269e275609ef",
+    "url": "/memory-game/static/media/Hades.6bb33165.jpg"
+  },
+  {
+    "revision": "b85fbc2bd35e0ab137f496efc6781de1",
+    "url": "/memory-game/static/media/Igor.b85fbc2b.jpg"
+  },
+  {
+    "revision": "8be0d8fc7f6dfb551cb6bff19798f3d5",
+    "url": "/memory-game/static/media/Joker.8be0d8fc.png"
+  },
+  {
+    "revision": "30e5963bb519dc58560524b5505c425a",
+    "url": "/memory-game/static/media/Kerrigan.30e5963b.jpg"
+  },
+  {
+    "revision": "55ee4972fe82e00b2f01f784d945a8d1",
+    "url": "/memory-game/static/media/Kong.55ee4972.jpg"
+  },
+  {
+    "revision": "582254f9c278ad5b88465f7b115422aa",
+    "url": "/memory-game/static/media/Lara.582254f9.jpeg"
+  },
+  {
+    "revision": "255257c52a933d1e531f6d2e3367508e",
+    "url": "/memory-game/static/media/Layton.255257c5.png"
+  },
+  {
+    "revision": "62df6feae76e504e8caa6859c9a0cd87",
+    "url": "/memory-game/static/media/Legion.62df6fea.png"
+  },
+  {
+    "revision": "2c5a643bea4c96ab738d1489e935fc0f",
+    "url": "/memory-game/static/media/Luxord.2c5a643b.png"
+  },
+  {
+    "revision": "3a1207640f360cfb13eb058837a291cb",
+    "url": "/memory-game/static/media/Mario.3a120764.jpg"
+  },
+  {
+    "revision": "9300418d50cb935620e9d96877965730",
+    "url": "/memory-game/static/media/Marluxia.9300418d.jpg"
+  },
+  {
+    "revision": "32ea7cda4279f79449c334e0ddae9cac",
+    "url": "/memory-game/static/media/Mercy.32ea7cda.jpg"
+  },
+  {
+    "revision": "432344f6dd088bc4fe8ee4af13d098fb",
+    "url": "/memory-game/static/media/Moira.432344f6.jpg"
+  },
+  {
+    "revision": "474398fde80bc073008185f470c4d2c5",
+    "url": "/memory-game/static/media/Moneybags.474398fd.jpg"
+  },
+  {
+    "revision": "7af6096c427d060aa993dd2f8d1a751b",
+    "url": "/memory-game/static/media/SAM.7af6096c.png"
+  },
+  {
+    "revision": "f8544f299305fd077b21c7f41b34e93b",
+    "url": "/memory-game/static/media/Scarecrow.f8544f29.jpg"
+  },
+  {
+    "revision": "92518f0abb23a89d0728b462fec0d58c",
+    "url": "/memory-game/static/media/Sephiroth.92518f0a.jpg"
+  },
+  {
+    "revision": "f68c1200e8d44c21facc8d65c0c8c774",
+    "url": "/memory-game/static/media/Soundwave.f68c1200.jpg"
+  },
+  {
+    "revision": "aa805bf76f6efc90f345c01e81ed8517",
+    "url": "/memory-game/static/media/Spiderman.aa805bf7.jpg"
+  },
+  {
+    "revision": "ec108d5c13ecf58099ac2e3c01112ebc",
+    "url": "/memory-game/static/media/Spyro.ec108d5c.jpg"
+  },
+  {
+    "revision": "986f3dadaaf7ebfdc122d3545d7b5c56",
+    "url": "/memory-game/static/media/Vivi.986f3dad.jpg"
+  },
+  {
+    "revision": "36633111686ef67c52aeed5aff5341de",
+    "url": "/memory-game/static/media/Wakka.36633111.jpg"
+  },
+  {
+    "revision": "6427ea69ef3107757ea034b18796e2da",
+    "url": "/memory-game/static/media/Yuna.6427ea69.jpg"
+  },
+  {
+    "revision": "8fb44fc89bea77757080ca7078bf3366",
+    "url": "/memory-game/index.html"
+  }
+];
